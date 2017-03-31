3e1d7b82709525c0a66fa6a51bfa4644624db8cd
iisquare <iisquare@163.com>
2017-03-31 18:01:29 +0800

    analytics-proxy

---
+++ demo/analytics-proxy/src/main/java/com/iisquare/etl/visual/analytics/Proxy.java
@@ -0,0 +1,18 @@
+package com.iisquare.etl.visual.analytics;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 统计代理服务端
+ * @author Ouyang <iisquare@163.com>
+ *
+ */
+public class Proxy {
+
+	static final Logger logger = Logger.getLogger(Proxy.class);
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
