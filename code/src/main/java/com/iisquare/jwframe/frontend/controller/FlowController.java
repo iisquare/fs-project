7df092d5111ef06dd6f2083b6668d0210c95ed95
iisquare <iisquare@163.com>
2017-06-16 11:04:25 +0800

    restore

--- code/src/main/java/com/iisquare/jwframe/frontend/controller/FlowController.java
+++ code/src/main/java/com/iisquare/jwframe/frontend/controller/FlowController.java
@@ -1,11 +1,14 @@
 package com.iisquare.jwframe.frontend.controller;
 
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import com.iisquare.jwframe.core.component.CoreController;
 import com.iisquare.jwframe.service.DemoService;
+import com.iisquare.jwframe.utils.FileUtil;
 
 @Controller
 @Scope("prototype")
@@ -13,17 +16,26 @@ public class FlowController extends CoreController {
 
 	@Autowired
 	public DemoService demoService;
+	private String flowJson = null;
 	
 	public Object demoAction() throws Exception {
 		return displayTemplate();
 	}
 	
 	public Object editAction() throws Exception {
+		if(null == flowJson) flowJson = FileUtil.getContent(rootPath + File.separator + "WEB-INF"
+			+ File.separator + "template" + File.separator + "frontend" + File.separator + "flow" + File.separator + "test.json");
+		assign("flowJson", flowJson);
 		return displayTemplate();
 	}
 	
 	public Object pluginsAction() throws Exception {
 		return displayTemplate();
+	}
+	
+	public Object saveAction() throws Exception {
+		flowJson = getParam("flow");
+		return displayMessage(0, null, null);
 	}
 
 }
