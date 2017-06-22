be5cd26075c5d8f35c6a5d29618e763d30721a28
iisquare <iisquare@163.com>
2017-06-22 16:01:08 +0800

    gradle

--- code/src/test/java/com/iisquare/jwframe/test/Tester.java
+++ code/src/test/java/com/iisquare/jwframe/test/Tester.java
@@ -1,14 +1,18 @@
 package com.iisquare.jwframe.test;
 
+import org.gradle.api.Project;
+import org.gradle.testfixtures.ProjectBuilder;
+
 import com.iisquare.etl.spark.flow.Submitter;
 import com.iisquare.jwframe.utils.FileUtil;
 
 public class Tester {
 
 	public static void main(String[] args) throws Exception {
-		String json = FileUtil.getContent("src/main/webapp/WEB-INF/template/frontend/flow/test.json");
+		/*String json = FileUtil.getContent("src/main/webapp/WEB-INF/template/frontend/flow/test.json");
 		boolean result = Submitter.submit(json, true);
-		System.out.println(result);
+		System.out.println(result);*/
+		Project project = ProjectBuilder.builder().build();
 	}
 
 }
