4934196b3f32f97044c9ba507a0ea3b25feea949
iisquare <iisquare@163.com>
2017-06-27 16:30:16 +0800

    client

--- code/spark/com/iisquare/etl/spark/flow/Submitter.java
+++ code/spark/com/iisquare/etl/spark/flow/Submitter.java
@@ -26,18 +26,34 @@ public class Submitter {
 		} else {
 			FlowService flowService = new FlowService();
 			Set<String> jarsSet = flowService.generateJars(config.getProperty("plugins.uri"), forceReload);
+			if(forceReload) flowService.generateDependencies(forceReload);
+			Set<String> packagesSet = flowService.generatePackages();
+			Set<String> excludeSet = flowService.generateExcludePackages();
+			String repositories = config.getProperty("maven.repositories");
 			List<String> argList = new ArrayList<>();
 			argList.add("--master");
 			argList.add(master);
 			argList.add("--name");
 			argList.add(appName);
 			argList.add("--deploy-mode");
-			argList.add("cluster");
+			argList.add(config.getProperty("deploy.mode", "client"));
 			argList.add("--class");
 			argList.add(TaskRunner.class.getName());
 			if(!jarsSet.isEmpty()) {
 				argList.add("--jars");
 				argList.add(DPUtil.implode(",", DPUtil.collectionToArray(jarsSet)));
+			}
+			if(!packagesSet.isEmpty()) {
+				argList.add("--packages");
+				argList.add(DPUtil.implode(",", DPUtil.collectionToArray(packagesSet)));
+			}
+			if(!excludeSet.isEmpty()) {
+				argList.add("--exclude-packages");
+				argList.add(DPUtil.implode(",", DPUtil.collectionToArray(excludeSet)));
+			}
+			if(!DPUtil.empty(repositories)) {
+				argList.add("--repositories");
+				argList.add(repositories);
 			}
 			if(verbose) argList.add("--verbose");
 			argList.add(config.getProperty("app.jar.url", "build/libs/etl-visual.jar"));
