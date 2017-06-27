039c6ae426eca1c6f86b149a6ba4381d1ead8c92
iisquare <iisquare@163.com>
2017-06-27 10:42:24 +0800

    Submitter

--- code/spark/com/iisquare/etl/spark/flow/Submitter.java
+++ code/spark/com/iisquare/etl/spark/flow/Submitter.java
@@ -30,6 +30,7 @@ public class Submitter {
 			if(forceReload) flowService.generateDependencies(forceReload);
 			Set<String> packagesSet = flowService.generatePackages();
 			Set<String> excludeSet = flowService.generateExcludePackages();
+			String repositories = config.getProperty("maven.repositories");
 			List<String> argList = new ArrayList<>();
 			argList.add("--master");
 			argList.add(master);
@@ -50,6 +51,10 @@ public class Submitter {
 			if(!excludeSet.isEmpty()) {
 				argList.add("--exclude-packages");
 				argList.add(DPUtil.implode(",", DPUtil.collectionToArray(excludeSet)));
+			}
+			if(!DPUtil.empty(repositories)) {
+				argList.add("--repositories");
+				argList.add(repositories);
 			}
 			argList.add(config.getProperty("app.jar.url", "build/libs/etl-visual.jar"));
 			argList.add(json);
