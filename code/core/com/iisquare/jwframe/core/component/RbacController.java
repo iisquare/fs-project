e9079d51ee84cd598b3854db02aa7277a650963f
iisquare <iisquare@163.com>
2017-06-29 18:33:39 +0800

    rbac

--- code/core/com/iisquare/jwframe/core/component/RbacController.java
+++ code/core/com/iisquare/jwframe/core/component/RbacController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import com.iisquare.jwframe.service.MenuService;
 import com.iisquare.jwframe.service.SettingService;
 import com.iisquare.jwframe.service.UserService;
+import com.iisquare.jwframe.utils.ServletUtil;
 
 @Controller
 @Scope("prototype")
@@ -15,10 +17,14 @@ public abstract class RbacController extends CoreController {
 	protected SettingService settingService;
 	@Autowired
 	protected UserService userService;
+	@Autowired
+	protected MenuService menuService;
 	
 	@Override
 	protected Object displayTemplate(String controller, String action) throws Exception {
 		assign("siteName", settingService.getProperty(null, "siteName", "系统名称未设置"));
+		String uri = ServletUtil.getFullUrl(request, false, true);
+		assign("menuTree", menuService.generateTree(appPath, null, uri, true));
 		return super.displayTemplate(controller, action);
 	}
 	
--- code/model/com/iisquare/jwframe/service/MenuService.java
+++ code/model/com/iisquare/jwframe/service/MenuService.java
@@ -2,9 +2,11 @@ package com.iisquare.jwframe.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -13,6 +15,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.iisquare.jwframe.dao.MenuDao;
 import com.iisquare.jwframe.mvc.ServiceBase;
+import com.iisquare.jwframe.utils.DPUtil;
 
 @Service
 @Scope("singleton")
@@ -34,12 +37,50 @@ public class MenuService extends ServiceBase {
 		return list;
 	}
 	
-	public List<Map<String, Object>> generateTree(String module, boolean forceReload) {
+	/**
+	 * 生成已选中菜单项集合
+	 */
+	public void generateChecked(Set<Object> checkedSet, Map<Object, Map<String, Object>> itemMap, Object id) {
+		if(DPUtil.empty(id) || checkedSet.contains(id)) return;
+		checkedSet.add(id);
+		Map<String, Object> item = itemMap.get(id);
+		if(null == item) return;
+		generateChecked(checkedSet, itemMap, item.get("parent_id"));
+	}
+	
+	public List<Map<String, Object>> generateTree(String webUrl, String module, String uri, boolean forceReload) {
 		if(null == module) module = "backend";
 		if(!forceReload && generateTree.containsKey(module)) return generateTree.get(module);
 		MenuDao menuDao = webApplicationContext.getBean(MenuDao.class);
 		Map<Object, Map<String, Object>> itemMap = menuDao.select("id,name,parent_id,url,target,icon,state")
 			.where("module=:module and status=1", ":module", module).orderBy("sort asc").all("id");
+		int id = 0; // 查找当前菜单项
+		for (Entry<Object, Map<String, Object>> entry : itemMap.entrySet()) {
+			Map<String, Object> value = entry.getValue();
+			System.out.println(value);
+			System.out.println("0".equals(id));
+			System.out.println("0".equals(value.get("parent_id")));
+			if(0 == id && 0 == DPUtil.parseInt(value.get("parent_id"))) id = DPUtil.parseInt(value.get("id"));
+			if(!uri.equals(value.get("url"))) continue;
+			id = DPUtil.parseInt(value.get("id"));
+			break;
+		}
+		// 生成已选中菜单集合
+		Set<Object> checkedSet = new HashSet<>();
+		generateChecked(checkedSet, itemMap, id);
+		// 处理菜单数据
+		for (Entry<Object, Map<String, Object>> entry : itemMap.entrySet()) {
+			Map<String, Object> value = entry.getValue();
+			value.put("checked", checkedSet.contains(value.get("id")));
+			String url = DPUtil.parseString(value.get("url"));
+			if(DPUtil.empty(url)) {
+				url = "javascript:void(0);";
+			} else if(!url.matches("^\\w+://.*$")) {
+				url = webUrl + url;
+			}
+			value.put("url", url);
+		}
+		// 生成树结构
 		List<Map<String, Object>> list = generateTree(itemMap, 0);
 		generateTree.put(module, list);
 		return list;
