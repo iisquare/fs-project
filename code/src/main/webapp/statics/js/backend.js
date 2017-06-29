3fe6fe52f5f704661fa1c3f1d026e98a3034f6ca
iisquare <iisquare@163.com>
2017-06-29 16:55:38 +0800

    rbac

--- code/src/main/webapp/statics/js/backend.js
+++ code/src/main/webapp/statics/js/backend.js
@@ -36,7 +36,14 @@ function Web_getDatagridCheckedIdArray($datagrid, idField) {
     }
     return idArray;
 }
-
+/**
+ * 格式化URL地址
+ */
+function Web_generateUrl(url) {
+    if(!url) return 'javascript:void(0);';
+    if(url.match(/^\w+:\/\/.*$/)) return url;
+    return webUrl + url;
+}
 $.fn.linkbuttonToggle = function () {
     var $obj = $(this);
     if(arguments.length) {
@@ -53,11 +60,47 @@ $.fn.linkbuttonToggle = function () {
     options.text = $obj.data('text');
     $obj.linkbutton(options);
 }
-$.ajax({
-    url : webUrl + '/menu/tree/',
-    dataType : 'json',
-    success : function (result) {
-        if(!result || 0 != result.code) return;
-
-    }
+$(function () {
+    var $platformMenuTop = $('#platform-menu-top');
+    var $platformMenuLeft = $('#platform-menu-left');
+    $.ajax({
+        url: webUrl + '/menu/tree/',
+        dataType: 'json',
+        success: function (result) {
+            if (!result || 0 != result.code) return;
+            var html = [];
+            for (var i in result.data) {
+                var item = result.data[i];
+                html.push('<li class="platform-menu-item" data-index="', i, '"><a href="',
+                    Web_generateUrl(item.url), '"><i class="', item.icon, '"></i> ', item.name, ' </a></li>');
+            }
+            $platformMenuTop.html(html.join(''));
+            $platformMenuTop.children('.platform-menu-item').on('click', function () {
+                var data = result.data[$(this).data('index')];
+                var html = [];
+                for (var i in data.children) {
+                    var item = data.children[i];
+                    html.push('<li class="platform-menu-item">');
+                    html.push('<a href="', Web_generateUrl(item.url), '" class="dropdown-toggle">');
+                    html.push('<i class="menu-icon ', item.icon, '"></i>');
+                    html.push('<span class="menu-text"> ', item.name, ' </span>');
+                    if(item.children.length > 0) html.push(' <b class="arrow fa fa-angle-down"></b>');
+                    html.push('</a><b class="arrow"></b>');
+                    if(item.children.length < 1) {
+                        html.push('</li>');
+                        continue;
+                    }
+                    html.push('<ul class="submenu">');
+                    for (var j in item.children) {
+                        var child = item.children[j];
+                        html.push('<li class="platform-menu-item"><a href="', Web_generateUrl(child.url), '">');
+                        html.push('<i class="menu-icon fa fa-caret-right"></i> ', child.name, ' </a>');
+                        html.push('<b class="arrow"></b></li>');
+                    }
+                    html.push('</ul></li>');
+                }
+                $platformMenuLeft.html(html.join(''));
+            }).eq(0).trigger('click');
+        }
+    });
 });
