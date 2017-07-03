75e2a28b7f583919d8440d34eda8531d03eb1002
iisquare <iisquare@163.com>
2017-07-03 11:07:16 +0800

    rbac user

--- code/model/com/iisquare/jwframe/service/UserService.java
+++ code/model/com/iisquare/jwframe/service/UserService.java
@@ -1,11 +1,21 @@
 package com.iisquare.jwframe.service;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.iisquare.jwframe.Configuration;
+import com.iisquare.jwframe.dao.UserDao;
 import com.iisquare.jwframe.mvc.ServiceBase;
+import com.iisquare.jwframe.utils.CodeUtil;
+import com.iisquare.jwframe.utils.DPUtil;
+import com.iisquare.jwframe.utils.ServiceUtil;
 
 @Service
 @Scope("singleton")
@@ -13,5 +23,94 @@ public class UserService extends ServiceBase {
 	
 	@Autowired
 	protected WebApplicationContext webApplicationContext;
+	@Autowired
+	protected Configuration configuration;
+	
+	public String generatePassword(String password, String salt) {
+		return CodeUtil.md5(password + salt);
+	}
+	
+	public Map<String, String> getStatusMap() {
+		Map<String, String> map = new LinkedHashMap<String, String>();
+		map.put("0", "禁用");
+		map.put("1", "正常");
+		return map;
+	}
+	
+	public int delete(Object ...ids) {
+		if(DPUtil.empty(ids)) return -1;
+		UserDao dao = webApplicationContext.getBean(UserDao.class);
+		return dao.where("id in ("
+			+ DPUtil.implode(",", DPUtil.arrayToIntegerArray(ids)) + ")", new HashMap<>()).delete().intValue();
+	}
+	
+	public Map<Object, Object> search(Map<String, Object> map, String orderBy, int page, int pageSize) {
+		StringBuilder condition = new StringBuilder("1=1");
+		Map<String, Object> params = new HashMap<String, Object>();
+		Object name = map.get("name");
+		if(!DPUtil.empty(name)) {
+			condition.append(" and name like :name");
+			params.put(":name", "%" + name + "%");
+		}
+		Object username = map.get("username");
+		if(!DPUtil.empty(username)) {
+			condition.append(" and username like :username");
+			params.put(":username", "%" + username + "%");
+		}
+		Object status = map.get("status");
+		if(null != status && !"".equals(status)) {
+			condition.append(" and status = :status");
+			params.put(":status", status);
+		}
+		Object description = map.get("description");
+		if(!DPUtil.empty(description)) {
+			condition.append(" and description like :description");
+			params.put(":description", "%" + description + "%");
+		}
+		Object timeCreateStart = map.get("timeCreateStart");
+		if(!DPUtil.empty(timeCreateStart)) {
+			condition.append(" and create_time >= :timeCreateStart");
+			params.put(":timeCreateStart", DPUtil.dateTimeToMillis(timeCreateStart, configuration.getDateTimeFormat()));
+		}
+		Object timeCreateEnd = map.get("timeCreateEnd");
+		if(!DPUtil.empty(timeCreateEnd)) {
+			condition.append(" and create_time <= :timeCreateEnd");
+			params.put(":timeCreateEnd", DPUtil.dateTimeToMillis(timeCreateEnd, configuration.getDateTimeFormat()));
+		}
+		Object timeUpdateStart = map.get("timeUpdateStart");
+		if(!DPUtil.empty(timeUpdateStart)) {
+			condition.append(" and update_time >= :timeUpdateStart");
+			params.put(":timeUpdateStart", DPUtil.dateTimeToMillis(timeUpdateStart, configuration.getDateTimeFormat()));
+		}
+		Object timeUpdateEnd = map.get("timeUpdateEnd");
+		if(!DPUtil.empty(timeUpdateEnd)) {
+			condition.append(" and update_time <= :timeUpdateEnd");
+			params.put(":timeUpdateEnd", DPUtil.dateTimeToMillis(timeUpdateEnd, configuration.getDateTimeFormat()));
+		}
+		UserDao dao = webApplicationContext.getBean(UserDao.class);
+		int total = dao.where(condition.toString(), params).count().intValue();
+		List<Map<String, Object>> rows = dao.orderBy(orderBy).page(page, pageSize).all();
+		rows = ServiceUtil.fillFields(rows, new String[]{"status"}, new Map<?, ?>[]{getStatusMap()}, null);
+		UserDao userDao = webApplicationContext.getBean(UserDao.class);
+		rows = ServiceUtil.fillRelations(rows, userDao,
+				new String[]{"create_uid", "update_uid"}, new String[]{"id", "username", "name"}, null);
+		return DPUtil.buildMap(new String[]{"total", "rows"}, new Object[]{total, rows});
+	}
+	
+	public Map<String, Object> getInfo(Object id) {
+		UserDao dao = webApplicationContext.getBean(UserDao.class);
+		return dao.where("id = :id", ":id", id).one();
+	}
+	
+	public int insert(Map<String, Object> data) {
+		if(DPUtil.empty(data.get("id"))) data.remove("id");
+		UserDao dao = webApplicationContext.getBean(UserDao.class);
+		return dao.insert(data).intValue();
+	}
+	
+	public int update(Map<String, Object> data) {
+		UserDao dao = webApplicationContext.getBean(UserDao.class);
+		return dao.where("id = :id", ":id", data.get("id")).update(data).intValue();
+	}
 
 }
--- code/src/main/java/com/iisquare/jwframe/backend/controller/UserController.java
+++ code/src/main/java/com/iisquare/jwframe/backend/controller/UserController.java
@@ -1,16 +1,96 @@
 package com.iisquare.jwframe.backend.controller;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import com.iisquare.jwframe.core.component.RbacController;
+import com.iisquare.jwframe.utils.DPUtil;
+import com.iisquare.jwframe.utils.ValidateUtil;
 
 @Controller
 @Scope("prototype")
 public class UserController extends RbacController {
 
 	public Object indexAction () throws Exception {
+		assign("qargs", params);
+		assign("statusMap", userService.getStatusMap());
 		return displayTemplate();
+	}
+	
+	public Object listAction () throws Exception {
+		int page = ValidateUtil.filterInteger(getParam("page"), true, 0, null, 1);
+		int pageSize = ValidateUtil.filterInteger(getParam("rows"), true, 0, 500, 30);
+		Map<Object, Object> map = userService.search(params, "sort asc, update_time desc", page, pageSize);
+		assign("total", map.get("total"));
+		assign("rows", DPUtil.collectionToArray((Collection<?>) map.get("rows")));
+		return displayJSON();
+	}
+	
+	public Object deleteAction() throws Exception {
+		Object[] idArray = getArray("ids");
+		int result = userService.delete(idArray);
+		if(-1 == result) return displayInfo(10001, "参数异常", null);
+		if(result >= 0) {
+			return displayInfo(0, null, url("index"));
+		} else {
+			return displayInfo(500, null, null);
+		}
+	}
+	
+	public Object editAction() throws Exception {
+		Integer id = ValidateUtil.filterInteger(getParam("id"), true, 0, null, null);
+		Map<String, Object> info;
+		if(null == id) {
+			info = new HashMap<>();
+		} else {
+			info = userService.getInfo(id);
+			if(null == info) return displayInfo(404, null, null);
+		}
+		assign("info", info);
+		assign("statusMap", userService.getStatusMap());
+		return displayTemplate();
+	}
+	
+	public Object saveAction() throws Exception {
+		String name = DPUtil.trim(getParam("name"));
+		if(DPUtil.empty(name)) return displayMessage(10001, "名称不能为空", null);
+		String username = DPUtil.trim(getParam("username"));
+		if(DPUtil.empty(username)) return displayMessage(10002, "账号不能为空", null);
+		String password = DPUtil.trim(getParam("password"));
+		long time = System.currentTimeMillis();
+		Map<String, Object> data = params;
+		data.put("name", name);
+		data.put("username", username);
+		data.put("sort", DPUtil.parseInt(getParam("sort")));
+		data.put("status", DPUtil.parseInt(getParam("status")));
+		data.put("update_uid", 0);
+		data.put("update_time", time);
+		int result = -1;
+		if(DPUtil.empty(getParam("id"))) {
+			data.put("create_uid", 0);
+			data.put("create_time", time);
+			if(DPUtil.empty(password)) {
+				password = settingService.getProperty(null, "defaultPassword", "admin888");
+			}
+			String salt = DPUtil.random(6);
+			data.put("salt", salt);
+			data.put("password", userService.generatePassword(password, salt));
+			result = userService.insert(data);
+			if(1 > result) return displayMessage(500, "添加失败", null);
+		} else {
+			if(!DPUtil.empty(password)) {
+				String salt = DPUtil.random(6);
+				data.put("salt", salt);
+				data.put("password", userService.generatePassword(password, salt));
+			}
+			result = userService.update(data);
+			if(0 > result) return displayMessage(500, "修改失败", null);
+		}
+		return displayMessage(0, null, result);
 	}
 	
 }
