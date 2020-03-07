b35787c42fcdc13a3da668a963533bc816b5d895
IISquare <iisquare@163.com>
2020-03-07 18:58:40 +0800

    feign

--- java/web/admin/src/main/java/com/iisquare/fs/web/admin/AdminApplication.java
+++ java/web/admin/src/main/java/com/iisquare/fs/web/admin/AdminApplication.java
@@ -1,6 +1,7 @@
 package com.iisquare.fs.web.admin;
 
 import com.iisquare.fs.base.web.mvc.BeanNameGenerator;
+import com.iisquare.fs.web.core.mvc.FeignInterceptor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -13,7 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 })
 @EnableFeignClients(basePackages = {
         "com.iisquare.fs.web.core.rpc"
-})
+}, defaultConfiguration = { FeignInterceptor.class })
 public class AdminApplication {
 
     public static void main(String[] args) {
