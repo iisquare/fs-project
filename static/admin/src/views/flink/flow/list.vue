6a0164d9c050927c30aeced885aff1cc05e71e40
IISquare <iisquare@163.com>
2020-04-01 17:27:37 +0800

    flink flow

--- static/admin/src/views/flink/flow/list.vue
+++ static/admin/src/views/flink/flow/list.vue
@@ -5,47 +5,21 @@
         <a-form-model ref="filters" :model="filters" layout="inline">
           <a-row :gutter="48">
             <a-col :md="6" :sm="24">
-              <a-form-model-item label="父级ID" prop="parentId">
-                <a-input v-model="filters.parentId" placeholder="" :allowClear="true" />
-              </a-form-model-item>
-            </a-col>
-            <a-col :md="6" :sm="24">
               <a-form-model-item label="名称" prop="name">
                 <a-input v-model="filters.name" placeholder="" :allowClear="true" />
               </a-form-model-item>
             </a-col>
             <a-col :md="6" :sm="24">
-              <a-form-model-item label="状态" prop="status">
-                <a-select v-model="filters.status" placeholder="请选择" :allowClear="true">
-                  <a-select-option v-for="(value, key) in config.status" :key="key" :value="key">{{ value }}</a-select-option>
+              <a-form-model-item label="类型" prop="status">
+                <a-select v-model="filters.type" placeholder="请选择" :allowClear="true">
+                  <a-select-option v-for="(value, key) in config.types" :key="key" :value="key">{{ value }}</a-select-option>
                 </a-select>
               </a-form-model-item>
             </a-col>
-            <template v-if="advanced">
-              <a-col :md="6" :sm="24">
-                <a-form-model-item label="类型" prop="type">
-                  <a-input v-model="filters.type" placeholder="" :allowClear="true" />
-                </a-form-model-item>
-              </a-col>
-              <a-col :md="6" :sm="24">
-                <a-form-model-item label="插件" prop="plugin">
-                  <a-input v-model="filters.plugin" placeholder="" :allowClear="true" />
-                </a-form-model-item>
-              </a-col>
-              <a-col :md="12" :sm="24">
-                <a-form-model-item label="类名" prop="classname">
-                  <a-input v-model="filters.classname" placeholder="" :allowClear="true" />
-                </a-form-model-item>
-              </a-col>
-            </template>
             <a-col :md="6" :sm="24">
               <span class="table-page-search-submitButtons" :style="advanced && { float: 'right', overflow: 'hidden' } || {} ">
                 <a-button type="primary" @click="search(true, false)" :loading="loading">查询</a-button>
                 <a-button style="margin-left: 8px" @click="() => this.$refs.filters.resetFields()">重置</a-button>
-                <a @click="toggleAdvanced" style="margin-left: 8px">
-                  {{ advanced ? '收起' : '展开' }}
-                  <a-icon :type="advanced ? 'up' : 'down'"/>
-                </a>
               </span>
             </a-col>
           </a-row>
@@ -65,29 +39,22 @@
             <a-button-group>
               <a-button type="link" size="small" v-permit="'flink:flow:'" @click="show(text, record)">查看</a-button>
               <a-button v-permit="'flink:flow:modify'" type="link" size="small" @click="edit(text, record)">编辑</a-button>
-              <a-button v-permit="'flink:flow:modify'" type="link" size="small" @click="sublevel(text, record)">子级</a-button>
+              <a-button v-permit="'flink:flow:modify'" type="link" size="small" @click="model(text, record)">模型</a-button>
             </a-button-group>
           </span>
         </a-table>
         <div :class="rows.length > 0 ? 'table-pagination-tools' : 'table-pagination-tools-empty'">
           <a-button icon="minus-circle" type="danger" @click="batchRemove" v-permit="'flink:flow:delete'" :disabled="selection.selectedRows.length === 0">删除</a-button>
           <a-divider type="vertical" v-permit="'flink:flow:add'" />
-          <a-button icon="plus-circle" type="primary" @click="add(0)" v-permit="'flink:flow:add'">新增</a-button>
+          <a-button icon="plus-circle" type="primary" @click="add" v-permit="'flink:flow:add'">新增</a-button>
         </div>
       </div>
     </a-card>
     <!--展示界面-->
     <a-modal :title="'信息查看 - ' + form.id" v-model="infoVisible" :footer="null">
       <a-form-model :model="form" :loading="infoLoading" :label-col="{ span: 5 }" :wrapper-col="{ span: 15 }">
-        <a-form-model-item label="父级">[{{ form.parentId }}]{{ form.parentId > 0 ? form.parentIdName : '根节点' }}</a-form-model-item>
         <a-form-model-item label="名称">{{ form.name }}</a-form-model-item>
-        <a-form-model-item label="全称">{{ form.fullName }}</a-form-model-item>
-        <a-form-model-item label="类型">{{ form.type }}</a-form-model-item>
-        <a-form-model-item label="插件">{{ form.plugin }}</a-form-model-item>
-        <a-form-model-item label="图标"><a-icon v-if="form.icon" :type="form.icon" />{{ form.icon }}</a-form-model-item>
-        <a-form-model-item label="展开">{{ form.state }}</a-form-model-item>
-        <a-form-model-item label="类名">{{ form.classname }}</a-form-model-item>
-        <a-form-model-item label="拖拽">{{ form.draggable ? '是' : '否' }}</a-form-model-item>
+        <a-form-model-item label="类型">{{ form.typeText }}</a-form-model-item>
         <a-form-model-item label="排序">{{ form.sort }}</a-form-model-item>
         <a-form-model-item label="状态">{{ form.statusText }}</a-form-model-item>
         <a-form-model-item label="描述">{{ form.description }}</a-form-model-item>
@@ -100,9 +67,6 @@
     <!--编辑界面-->
     <a-modal :title="'信息' + (form.id ? ('修改 - ' + form.id) : '添加')" v-model="formVisible" :confirmLoading="formLoading" :maskClosable="false" @ok="submit">
       <a-form-model ref="form" :model="form" :rules="rules" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
-        <a-form-model-item label="父级" prop="parentId">
-          <a-input v-model="form.parentId" auto-complete="off"></a-input>
-        </a-form-model-item>
         <a-form-model-item label="ID" prop="id">
           <a-input v-model="form.id" auto-complete="off"></a-input>
         </a-form-model-item>
@@ -110,28 +74,9 @@
           <a-input v-model="form.name" auto-complete="off"></a-input>
         </a-form-model-item>
         <a-form-model-item label="类型" prop="type">
-          <a-input v-model="form.type" auto-complete="off"></a-input>
-        </a-form-model-item>
-        <a-form-model-item label="插件" prop="plugin">
-          <a-input v-model="form.plugin" auto-complete="off"></a-input>
-        </a-form-model-item>
-        <a-form-model-item label="图标" prop="icon">
-          <a-input v-model="form.icon" auto-complete="off"></a-input>
-        </a-form-model-item>
-        <a-form-model-item label="展开状态" prop="state">
-          <a-input v-model="form.state" auto-complete="off"></a-input>
-        </a-form-model-item>
-        <a-form-model-item label="类名" prop="classname">
-          <a-input v-model="form.classname" auto-complete="off"></a-input>
-        </a-form-model-item>
-        <a-form-model-item label="拖拽" prop="draggable">
-          <a-checkbox v-model="form.draggable">允许拖拽</a-checkbox>
-        </a-form-model-item>
-        <a-form-model-item label="属性" prop="property">
-          <a-textarea v-model="form.property"></a-textarea>
-        </a-form-model-item>
-        <a-form-model-item label="返回值" prop="returns">
-          <a-textarea v-model="form.returns"></a-textarea>
+          <a-select v-model="form.type" placeholder="请选择">
+            <a-select-option v-for="(value, key) in config.types" :key="key" :value="key">{{ value }}</a-select-option>
+          </a-select>
         </a-form-model-item>
         <a-form-model-item label="排序">
           <a-input-number v-model="form.sort" :min="0" :max="200"></a-input-number>
@@ -143,6 +88,9 @@
         </a-form-model-item>
         <a-form-model-item label="描述">
           <a-textarea v-model="form.description"></a-textarea>
+        </a-form-model-item>
+        <a-form-model-item label="内容">
+          <a-textarea v-model="form.content"></a-textarea>
         </a-form-model-item>
       </a-form-model>
     </a-modal>
@@ -162,10 +110,7 @@ export default {
       columns: [
         { title: 'ID', dataIndex: 'id' },
         { title: '名称', dataIndex: 'name' },
-        { title: '全称', dataIndex: 'fullName' },
-        { title: '父级', dataIndex: 'parentId', scopedSlots: { customRender: 'parentId' } },
-        { title: '分类', dataIndex: 'type' },
-        { title: '插件', dataIndex: 'plugin' },
+        { title: '类型', dataIndex: 'typeText' },
         { title: '排序', dataIndex: 'sort' },
         { title: '状态', dataIndex: 'statusText' },
         { title: '操作', scopedSlots: { customRender: 'action' } }
@@ -190,6 +135,9 @@ export default {
     }
   },
   methods: {
+    model (id, row) {
+      this.$router.push('/flink/flow/model?id=' + row.id)
+    },
     dateRender (text, record, index) {
       return DateUtil.format(text)
     },
@@ -238,23 +186,18 @@ export default {
         })
       })
     },
-    sublevel (text, record) {
-      this.add(record.id)
-    },
-    add (parentId = 0) {
-      this.form = { parentId }
+    add () {
+      this.form = {}
       this.formVisible = true
     },
     edit (text, record) {
       this.form = Object.assign({}, record, {
-        status: record.status + '',
-        draggable: record.draggable === 1
+        status: record.status + ''
       })
       this.formVisible = true
     },
     show (text, record) {
       this.form = Object.assign({}, record, {
-        draggable: record.draggable === 1,
         description: record.description ? record.description : '暂无'
       })
       this.infoVisible = true
